469fd094057b8a84399d7f5a9f2c88e5ad0a91a9
Louis Bertson <louisbertson@gmail.com>
2024-11-12 10:39:32 +0000

    Added token types enum

--- src/scanner.rs
+++ src/scanner.rs
@@ -12,11 +12,56 @@ impl Scanner {
 
         //return vec!(Token{ name : String::from("Test")})
 
-        return Ok(vec!(Token{ name: String::from("Test")}))
+        return Ok(vec!(Token::LEFT_BRACE, Token::RIGHT_BRACE))
     }
 }
 
 #[derive(Debug)]
-pub struct Token {
-    name: String
+pub enum Token {
+    // Single-char tokens
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    LEFT_BRACE,
+    RIGHT_BRACE,
+    COMMA,
+    DOT,
+    MINUS,
+    PLUS,
+    SEMICOLON,
+    SLASH,
+    START,
+
+    // One or Two chars
+    BANG,
+    BANG_EQUAL,
+    EQUAL,
+    EQUAL_EQUAL,
+    GREATER,
+    GREATER_EQUAL,
+    LESS,
+    LESS_EQUAL,
+
+    // Litterals
+    IDENTIFIER,
+    STRING,
+    NUMBER,
+
+    // Keywords
+    AND,
+    CLASS,
+    ELSE,
+    FALSE,
+    FUN,
+    FOR,
+    IF,
+    nil,
+    OR,
+    PRINT,
+    RETURN,
+    SUPER,
+    THIS,
+    TRUE,
+    VAR,
+    WHILE,
+    EOF   
 }
